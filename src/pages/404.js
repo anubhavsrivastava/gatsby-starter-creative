d7ab2252e76552a42040dc5ee47bf0716a367480
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-06-26 13:37:01 +0530

    updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,22 +2,32 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 import Header from '../components/Header';
+import { Link } from 'gatsby';
 
 const IndexPage = () => (
   <Layout>
     <Header />
     <header className="masthead">
-      <div className="container d-flex h-100 align-items-center">
-        <div className="mx-auto text-center">
-          <h1 className="mx-auto my-0 text-uppercase">Page not found</h1>
-          <h2 className="text-white-50 mx-auto mt-2 mb-5">Not a valid URL</h2>
-
-          <a href="/" className="btn btn-primary">
-            Go Home
-          </a>
+      <div className="container h-100">
+        <div className="row h-100 align-items-center justify-content-center text-center">
+          <div className="col-lg-10 align-self-end">
+            <h1 className="text-uppercase text-white font-weight-bold">
+              Page not found
+            </h1>
+            <hr className="divider my-4" />
+          </div>
+          <div className="col-lg-8 align-self-baseline">
+            <p className="text-white-75 font-weight-light mb-5">
+              Not a valid URL
+            </p>
+            <Link to="/" className="btn btn-primary btn-xl js-scroll-trigger">
+              Go Home
+            </Link>
+          </div>
         </div>
       </div>
     </header>
+    >
   </Layout>
 );
 
