3db5f210626af6497f58acaa8f4193a02aad5766
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-06-26 13:38:12 +0530

    updated site name

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -24,7 +24,7 @@ class Layout extends Component {
             <Helmet
               title={data.site.siteMetadata.title}
               meta={[
-                { name: 'description', content: 'Stylish' },
+                { name: 'description', content: 'Creative' },
                 { name: 'keywords', content: 'site, web' },
               ]}
             >
